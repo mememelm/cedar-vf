6c82b49fc318d7386dd3437964387a31fd57c88a
mememelm <carol.arivony@gmail.com>
2020-12-13 18:40:35 +0300

    length dash production

--- src/app/pages/production/bovin/bovin.component.ts
+++ src/app/pages/production/bovin/bovin.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { DatatableLanguage } from 'app/constant/french';
+import { DomaineService } from 'app/services/domaine.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-bovin',
@@ -7,9 +12,159 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BovinComponent implements OnInit {
 
-  constructor() { }
+  @Output() emitData: EventEmitter<any> = new EventEmitter()
+
+  public listDomaine: any
+  public listBovin: any
+  public dtOptions: any = {}
+
+  constructor(
+    private angularFireDatabase: AngularFireDatabase,
+    private spinner: NgxSpinnerService,
+    private domaineService: DomaineService
+  ) { }
 
   ngOnInit(): void {
+    this.dtOptions = {
+      language: DatatableLanguage.datatableFrench
+    }
+
+    this.domaineService.getDomaineList()
+      .snapshotChanges().pipe(map(changes => changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))))
+      .subscribe((res: any) => {
+        this.listDomaine = res
+      })
+
+    this.loadBovin()
+  }
+
+  /**
+   * returnDoSelection
+   */
+  public returnDoSelection(data) {
+    this.emitData.emit(data)
+  }
+
+  /**
+   * loadBovin
+   */
+  public loadBovin() {
+    this.spinner.show()
+
+    const array = []
+    const operator = this.angularFireDatabase.database.ref().child('exploitants')
+    const bovin = this.angularFireDatabase.database.ref().child('Bovin')
+
+    operator.on('child_added', snap => {
+      const exploitantId = snap.val().exploitantId
+      const exploitantFirstName = snap.val().exploitantFirstName
+      const exploitantLastName = snap.val().exploitantLastName
+      const exploitantCedar = snap.val().exploitantCedar
+      const exploitantDate = snap.val().exploitantDate
+      const exploitantSex = snap.val().exploitantSex
+      const exploitantDomaine = snap.val().exploitantDomaine
+      const exploitantAge = snap.val().exploitantAge
+      const exploitantCommune = snap.val().exploitantCommune
+      const exploitantDistrict = snap.val().exploitantDistrict
+      const exploitantFokotany = snap.val().exploitantFokotany
+      const exploitantRegion = snap.val().exploitantRegion
+      const exploitantActivite1 = snap.val().exploitantActivite1
+      const exploitantActivite2 = snap.val().exploitantActivite2
+      const exploitantSpeciality1 = snap.val().exploitantSpeciality1
+      const exploitantSpeciality2 = snap.val().exploitantSpeciality2
+      const exploitantFormation = snap.val().exploitantFormation
+      const exploitantSchooling = snap.val().exploitantSchooling
+
+      bovin.orderByChild('exploitantId').equalTo(exploitantId).on('value', snapshot => {
+        snapshot.forEach((res: any) => {
+          array.push({
+            exploitantId,
+            exploitantFirstName,
+            exploitantLastName,
+            exploitantCedar,
+            exploitantDate,
+            exploitantSex,
+            exploitantDomaine,
+            exploitantAge,
+            exploitantCommune,
+            exploitantDistrict,
+            exploitantFokotany,
+            exploitantRegion,
+            exploitantActivite1,
+            exploitantActivite2,
+            exploitantSpeciality1,
+            exploitantSpeciality2,
+            exploitantFormation,
+            exploitantSchooling,
+            ...res.val()
+          })
+
+          this.listBovin = array
+          this.spinner.hide()
+
+        })
+      })
+    })
+  }
+
+  /**
+   * filterMarketFood
+   */
+  public filterBovin(param) {
+    const array = []
+    const operator = this.angularFireDatabase.database.ref().child('exploitants')
+    const bovin = this.angularFireDatabase.database.ref().child('Bovin')
+
+    operator.on('child_added', snap => {
+      const exploitantId = snap.val().exploitantId
+      const exploitantFirstName = snap.val().exploitantFirstName
+      const exploitantLastName = snap.val().exploitantLastName
+      const exploitantCedar = snap.val().exploitantCedar
+      const exploitantDate = snap.val().exploitantDate
+      const exploitantSex = snap.val().exploitantSex
+      const exploitantDomaine = snap.val().exploitantDomaine
+      const exploitantAge = snap.val().exploitantAge
+      const exploitantCommune = snap.val().exploitantCommune
+      const exploitantDistrict = snap.val().exploitantDistrict
+      const exploitantFokotany = snap.val().exploitantFokotany
+      const exploitantRegion = snap.val().exploitantRegion
+      const exploitantActivite1 = snap.val().exploitantActivite1
+      const exploitantActivite2 = snap.val().exploitantActivite2
+      const exploitantSpeciality1 = snap.val().exploitantSpeciality1
+      const exploitantSpeciality2 = snap.val().exploitantSpeciality2
+      const exploitantFormation = snap.val().exploitantFormation
+      const exploitantSchooling = snap.val().exploitantSchooling
+
+      bovin.orderByChild('exploitantId').equalTo(exploitantId).on('value', snapshot => {
+        snapshot.forEach((res: any) => {
+          if (exploitantCedar == param) {
+            array.push({
+              exploitantId,
+              exploitantFirstName,
+              exploitantLastName,
+              exploitantCedar,
+              exploitantDate,
+              exploitantSex,
+              exploitantDomaine,
+              exploitantAge,
+              exploitantCommune,
+              exploitantDistrict,
+              exploitantFokotany,
+              exploitantRegion,
+              exploitantActivite1,
+              exploitantActivite2,
+              exploitantSpeciality1,
+              exploitantSpeciality2,
+              exploitantFormation,
+              exploitantSchooling,
+              ...res.val()
+            })
+
+            this.listBovin = array
+          }
+        })
+      })
+    })
   }
 
 }
--- src/app/pages/production/production.component.ts
+++ src/app/pages/production/production.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-production',
@@ -9,10 +11,23 @@ export class ProductionComponent implements OnInit {
 
   public navigation: any
 
-  constructor() { }
+  public cuma: any
+  public cuvi: any
+  public api: any
+  public avi: any
+  public cunni: any
+  public bovin: any
+  public porci: any
+
+  constructor(
+    private angularFireDatabase: AngularFireDatabase,
+    private spinner: NgxSpinnerService
+  ) { }
 
   ngOnInit(): void {
     this.navigation = 'menu'
+
+    this.loadLength()
   }
 
   /**
@@ -27,5 +42,121 @@ export class ProductionComponent implements OnInit {
    */
   public returnToProduction() {
     this.navigation = 'menu'
+  }
+
+  // number menu
+  public loadLength() {
+
+    this.spinner.show()
+
+    const arrayCuvi = []
+    const arrayCuma = []
+    const arrayCunni = []
+    const arrayApi = []
+    const arrayAvy = []
+    const arrayBovin = []
+    const arrayPorci = []
+
+    const operator = this.angularFireDatabase.database.ref().child('exploitants')
+
+    const cuvi = this.angularFireDatabase.database.ref().child('FoodCulture')
+    operator.on('child_added', snap => {
+      const exploitantId = snap.val().exploitantId
+      cuvi.orderByChild('exploitantId').equalTo(exploitantId).on('value', snapshot => {
+        snapshot.forEach((res: any) => {
+          arrayCuvi.push({
+            exploitantId,
+            ...res.val()
+          })
+          this.cuvi = arrayCuvi.length
+        })
+      })
+    })
+
+    const cuma = this.angularFireDatabase.database.ref().child('MarketCulture')
+    operator.on('child_added', snap => {
+      const exploitantId = snap.val().exploitantId
+      cuma.orderByChild('exploitantId').equalTo(exploitantId).on('value', snapshot => {
+        snapshot.forEach((res: any) => {
+          arrayCuma.push({
+            exploitantId,
+            ...res.val()
+          })
+          this.cuma = arrayCuma.length
+        })
+      })
+    })
+
+    const cunni = this.angularFireDatabase.database.ref().child('Cunniculture')
+    operator.on('child_added', snap => {
+      const exploitantId = snap.val().exploitantId
+      cunni.orderByChild('exploitantId').equalTo(exploitantId).on('value', snapshot => {
+        snapshot.forEach((res: any) => {
+          arrayCunni.push({
+            exploitantId,
+            ...res.val()
+          })
+          this.cunni = arrayCunni.length
+        })
+      })
+    })
+
+    const api = this.angularFireDatabase.database.ref().child('Apiculture')
+    operator.on('child_added', snap => {
+      const exploitantId = snap.val().exploitantId
+      api.orderByChild('exploitantId').equalTo(exploitantId).on('value', snapshot => {
+        snapshot.forEach((res: any) => {
+          arrayApi.push({
+            exploitantId,
+            ...res.val()
+          })
+          this.api = arrayApi.length
+        })
+      })
+    })
+
+    const avi = this.angularFireDatabase.database.ref().child('Aviculture')
+    operator.on('child_added', snap => {
+      const exploitantId = snap.val().exploitantId
+      avi.orderByChild('exploitantId').equalTo(exploitantId).on('value', snapshot => {
+        snapshot.forEach((res: any) => {
+          arrayAvy.push({
+            exploitantId,
+            ...res.val()
+          })
+          this.avi = arrayAvy.length
+        })
+      })
+    })
+
+    const bovin = this.angularFireDatabase.database.ref().child('Bovin')
+    operator.on('child_added', snap => {
+      const exploitantId = snap.val().exploitantId
+      bovin.orderByChild('exploitantId').equalTo(exploitantId).on('value', snapshot => {
+        snapshot.forEach((res: any) => {
+          arrayBovin.push({
+            exploitantId,
+            ...res.val()
+          })
+          this.bovin = arrayBovin.length
+        })
+      })
+    })
+
+    const porci = this.angularFireDatabase.database.ref().child('Poriculture')
+    operator.on('child_added', snap => {
+      const exploitantId = snap.val().exploitantId
+      porci.orderByChild('exploitantId').equalTo(exploitantId).on('value', snapshot => {
+        snapshot.forEach((res: any) => {
+          arrayPorci.push({
+            exploitantId,
+            ...res.val()
+          })
+          this.porci = arrayPorci.length
+        })
+      })
+    })
+
+    this.spinner.hide()
   }
 }
